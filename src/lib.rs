b13c71b110f60c395b0d80d805d1aa5250354c74
rLannes <romain.lannes@gmail.com>
2024-08-20 09:59:22 -0400

    bug fixes

--- src/lib.rs
+++ src/lib.rs
@@ -177,7 +177,7 @@ pub mod cigar{
                             ref_pos += n;
                             results.push(ref_pos);
                             },
-                        CigarOperation::Match(n) | CigarOperation::Insertion(n)  => { ref_pos += n; }, 
+                        CigarOperation::Match(n) | CigarOperation::Deletion(n)  => { ref_pos += n; }, 
                         CigarOperation::Soft(n) => (), // I am confuse by the difference here from IGV and the specification of CIGAR
                         _  => ()
                     }
